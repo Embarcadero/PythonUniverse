3c263dd2e73f53b376f36b207b6139fe130fd88e
fusionchartsexpress <FMXExpress@users.noreply.github.com>
2022-07-22 11:29:16 -0700

    Updated launcher.
    Changed launcher to download and install some files instead of just launching a URL.

--- uMainForm.pas
+++ uMainForm.pas
@@ -5,30 +5,43 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Imaging.jpeg,
-  Vcl.ExtCtrls, DosCommand, Vcl.Imaging.pngimage;
+  Vcl.ExtCtrls, DosCommand, System.Net.URLClient, System.Net.HttpClient,
+  System.Net.HttpClientComponent, Vcl.ComCtrls, System.IOUtils;
 
 type
   TMainForm = class(TForm)
+    UniverseImage: TImage;
     PyScripterButton: TButton;
     VideoTutorialsButton: TButton;
     eBookButton: TButton;
+    PythonButton: TButton;
     RADStudioButton: TButton;
     DelphiVCLButton: TButton;
     DelphiFMXButton: TButton;
     CMD: TDosCommand;
-    UniverseImage: TImage;
-    Button1: TButton;
-    Image1: TImage;
+    NetHTTPClient: TNetHTTPClient;
+    ProgressBar: TProgressBar;
     procedure PyScripterButtonClick(Sender: TObject);
     procedure VideoTutorialsButtonClick(Sender: TObject);
     procedure eBookButtonClick(Sender: TObject);
     procedure DelphiVCLButtonClick(Sender: TObject);
     procedure DelphiFMXButtonClick(Sender: TObject);
     procedure RADStudioButtonClick(Sender: TObject);
-    procedure RADExporterButtonClick(Sender: TObject);
-    procedure Python4DelphiButtonClick(Sender: TObject);
+    procedure PythonButtonClick(Sender: TObject);
+    procedure NetHTTPClientRequestCompleted(const Sender: TObject;
+      const AResponse: IHTTPResponse);
+    procedure NetHTTPClientRequestError(const Sender: TObject;
+      const AError: string);
+    procedure NetHTTPClientRequestException(const Sender: TObject;
+      const AError: Exception);
+    procedure NetHTTPClientReceiveData(const Sender: TObject; AContentLength,
+      AReadCount: Int64; var AAbort: Boolean);
   private
     { Private declarations }
+    FFilename: String;
+    procedure AsyncInstall(AURL: string; AFilename: string);
+    procedure EnableDownloads(AMessage: string);
+    procedure DisableDownloads;
   public
     { Public declarations }
   end;
@@ -40,51 +53,112 @@ implementation
 
 {$R *.dfm}
 
-procedure TMainForm.Python4DelphiButtonClick(Sender: TObject);
+procedure TMainForm.AsyncInstall(AURL: string; AFilename: string);
 begin
-  CMD.CommandLine := 'cmd /c "start https://github.com/pyscripter/MultiInstaller/blob/master/Bin/MultiInstaller.exe"';
-  CMD.Execute;
+  DisableDownloads;
+  NetHTTPClient.Asynchronous := True;
+  FFilename := TPath.Combine(TPath.GetDownloadsPath,AFilename);
+  NetHTTPClient.Get(AURL);
+end;
+
+procedure TMainForm.EnableDownloads(AMessage: string);
+begin
+  MainForm.Caption := MainForm.Hint + ': ' + AMessage;
+  ProgressBar.Visible := False;
+  PyScripterButton.Enabled := True;
+  VideoTutorialsButton.Enabled := True;
+  eBookButton.Enabled := True;
+  PythonButton.Enabled := True;
+  RADStudioButton.Enabled := True;
+  DelphiVCLButton.Enabled := True;
+  DelphiFMXButton.Enabled := True;
+end;
+
+procedure TMainForm.DisableDownloads;
+begin
+  PyScripterButton.Enabled := False;
+  VideoTutorialsButton.Enabled := False;
+  eBookButton.Enabled := False;
+  PythonButton.Enabled := False;
+  RADStudioButton.Enabled := False;
+  DelphiVCLButton.Enabled := False;
+  DelphiFMXButton.Enabled := False;
+  ProgressBar.Visible := True;
 end;
 
 procedure TMainForm.DelphiFMXButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "pip install delphifmx"';
+  CMD.CommandLine := 'cmd /c "'+DelphiFMXButton.Hint+'"';
   CMD.Execute;
 end;
 
 procedure TMainForm.DelphiVCLButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "pip install delphivcl"';
+  CMD.CommandLine := 'cmd /c "'+DelphiVCLButton.Hint+'"';
   CMD.Execute;
 end;
 
 procedure TMainForm.eBookButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "start https://lp.embarcadero.com/PythonGUIgettingStarted?utm_source=launcher&utm_medium=link&utm_campaign=python"';
+  AsyncInstall(eBookButton.Hint,'Getting_Started_with_Python_GUI_Development.pdf');
+end;
+
+procedure TMainForm.NetHTTPClientReceiveData(const Sender: TObject;
+  AContentLength, AReadCount: Int64; var AAbort: Boolean);
+begin
+  TThread.Synchronize(nil,procedure begin
+    ProgressBar.StepIt;
+  end);
+end;
+
+procedure TMainForm.NetHTTPClientRequestCompleted(const Sender: TObject;
+  const AResponse: IHTTPResponse);
+begin
+  var LMemoryStream := TMemoryStream.Create;
+  try
+    LMemoryStream.LoadFromStream(AResponse.ContentStream);
+    LMemoryStream.SaveToFile(FFilename);
+  finally
+    LMemoryStream.Free;
+  end;
+
+  EnableDownloads('Download complete!');
+
+  CMD.CommandLine := 'cmd /c "'+FFilename+'"';
   CMD.Execute;
+end;
+
+procedure TMainForm.NetHTTPClientRequestError(const Sender: TObject;
+  const AError: string);
+begin
+  EnableDownloads(AError);
+end;
+
+procedure TMainForm.NetHTTPClientRequestException(const Sender: TObject;
+  const AError: Exception);
+begin
+  EnableDownloads(AError.Message);
 end;
 
 procedure TMainForm.PyScripterButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "start https://sourceforge.net/projects/pyscripter/files/latest/download"';
-  CMD.Execute;
+  ASyncInstall(PyScripterButton.Hint,'PyScripter-4.1.1-x86-Setup.exe');
 end;
 
-procedure TMainForm.RADExporterButtonClick(Sender: TObject);
+procedure TMainForm.PythonButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "start https://github.com/Embarcadero/Delphi4PythonExporter/releases"';
-  CMD.Execute;
+  ASyncInstall(PythonButton.Hint,'python-3.10.5-amd64.exe');
 end;
 
 procedure TMainForm.RADStudioButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "start https://www.embarcadero.com/products/rad-studio/start-for-free?utm_source=launcher&utm_medium=link&utm_campaign=python"';
+  CMD.CommandLine := 'cmd /c "start '+RADStudioButton.Hint+'"';
   CMD.Execute;
 end;
 
 procedure TMainForm.VideoTutorialsButtonClick(Sender: TObject);
 begin
-  CMD.CommandLine := 'cmd /c "start https://www.youtube.com/playlist?list=PLwUPJvR9mZHiD7yZTZrArAjb0yLsRoZP9"';
+  CMD.CommandLine := 'cmd /c "start '+VideoTutorialsButton.Hint+'"';
   CMD.Execute;
 end;
 
